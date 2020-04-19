945643cb485f5c14963c7a75e15ec8e188e8e33d
CurtisNewbie <zhuangyongj@gmail.com>
2020-04-19 23:27:16 +0800

    Add logger in RoomResources

--- chatapp/src/main/java/com/curtisnewbie/chat/RoomResources.java
+++ chatapp/src/main/java/com/curtisnewbie/chat/RoomResources.java
@@ -5,6 +5,8 @@ import javax.inject.Inject;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 
+import org.jboss.logging.Logger;
+
 /**
  * Endpoint for retriving room keys for websocekt connections
  */
@@ -15,8 +17,12 @@ public class RoomResources {
     @Inject
     protected Rooms rooms;
 
+    @Inject
+    protected Logger logger;
+
     @GET
     public String getNewRoomKey() {
+        logger.info("Room created");
         return rooms.createRoom();
     }
 }

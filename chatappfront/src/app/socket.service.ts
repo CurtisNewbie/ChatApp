214d009976bca35dd7cab541c48149d5ea298071
CurtisNewbie <zhuangyongj@gmail.com>
2020-04-20 16:02:50 +0800

    Refactor socket.service.ts

--- chatappfront/src/app/socket.service.ts
+++ chatappfront/src/app/socket.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { CONFIG } from 'src/environments/config';
+import { webSocket, WebSocketSubject } from 'rxjs/webSocket';
+import { Observable } from 'rxjs';
 
 const FETCH_KEY_URL = `http://${CONFIG.host}:${CONFIG.port}/api/room/key`;
 
@@ -8,36 +10,29 @@ const FETCH_KEY_URL = `http://${CONFIG.host}:${CONFIG.port}/api/room/key`;
   providedIn: 'root',
 })
 export class SocketService {
-  private roomKey: string;
-  private username: string;
+  private wsConn: WebSocketSubject<string> = null;
 
   constructor(private http: HttpClient) {}
 
   /**
-   * Fetch the key of a new room from backend for socket connection
+   * Fetch the key of a new room from backend for websocket connection
    */
-  fetchRoomKey(): void {
+  fetchRoomKey(): Observable<string> {
     console.log(FETCH_KEY_URL);
-    let observable = this.http.get(FETCH_KEY_URL, { responseType: 'text' });
-    observable.subscribe({
-      next: (v) => {
-        this.roomKey = v;
-      },
-      error: (e) => {
-        console.log(e);
-      },
-      complete: () => {
-        console.log(this.roomKey);
-      },
-    });
+    return this.http.get(FETCH_KEY_URL, { responseType: 'text' });
   }
 
   /**
-   * Set room key for socket connection (if the key is known in advance)
+   * Open websocket connection and return an Observable of string
    *
-   * @param key
+   * @returns Observable of string or NULL if either the username or the roomKey is null or empty
    */
-  setRoomKey(key: string) {
-    this.roomKey = key;
+  openWsConn(username: string, roomKey: string): Observable<string> {
+    if (username && roomKey) {
+      this.wsConn = webSocket(
+        `ws://${CONFIG.host}:${CONFIG.port}/chat/room/${roomKey}/name/${username}`
+      );
+      return this.wsConn.asObservable();
+    }
   }
 }
